cf9e1917ca29b6c02483f34f665cbb397af57154
fiopans1 <dsuaran@gmail.com>
2022-03-27 12:27:02 +0200

    CASI TERMINADO

--- p1/p1.c
+++ p1/p1.c
@@ -13,45 +13,56 @@ int main(int argc, char *argv[])
     double PI25DT = 3.141592653589793238462643;
     double pi, x, y, z;
     MPI_Status status;
+    int rep,acu,fin;
 
     while (!done)
     {   if(rank==0){
-        printf("Enter the number of points: (0 quits) \n");
-        scanf("%d",&n);
-        int rep,rep1;
-        rep=n/numprocs;
-        rep1=rep;
-        for(int i=1;i<numprocs;i++){
-            if(n==0){
+            printf("Enter the number of points: (0 quits) \n");
+            scanf("%d",&n);
+            if(n!=0){
+               rep=n/numprocs; 
+            }else{
+                rep=0;
+            }
+            for(int i=1;i<numprocs;i++){
+                MPI_Send(&rep,1,MPI_INT,i,0,MPI_COMM_WORLD);
+                acu=acu+rep;
+            }
+            rep=n-acu;//para los que queden
 
-            }
-            else if(rep1<n){
-                MPI_Send(&rep1,1,MPI_INT,i,0,MPI_COMM_WORLD);
-            }
         }else{
-            //poner receive
+            MPI_Recv(&rep,1,MPI_INT,0,0,MPI_COMM_WORLD,&status);
         }
-        //simplemente repartir hasta n, y el resto que lo hagas el proceso 0, y al final que todos envien el numero de puntos al proceso 0
-        if (n == 0) break;
+        if (rep == 0) break;
 
         count = 0;  
 
-        for (i = 1; i <= n; i++) {
+        for (i = 1; i <= rep; i++) {
             // Get the random numbers between 0 and 1
-	    x = ((double) rand()) / ((double) RAND_MAX);
-	    y = ((double) rand()) / ((double) RAND_MAX);
+            x = ((double) rand()) / ((double) RAND_MAX);
+            y = ((double) rand()) / ((double) RAND_MAX);
 
-	    // Calculate the square root of the squares
-	    z = sqrt((x*x)+(y*y));
+            // Calculate the square root of the squares
+            z = sqrt((x*x)+(y*y));
 
-	    // Check whether z is within the circle
-	    if(z <= 1.0)
+            // Check whether z is within the circle
+            if(z <= 1.0){
                 count++;
+            }
+
         }
 
+        if(rank==0){
+            for(int i=1;i<numprocs;i++){
+                MPI_Recv(&fin,1,MPI_INT,i,0,MPI_COMM_WORLD,&status);
+                count+=fin;
+            }
+            pi = ((double) count/(double) n)*4.0;
 
-        pi = ((double) count/(double) n)*4.0;
+            printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        }else{
+            MPI_Send(&count,1,MPI_INT,0,0,MPI_COMM_WORLD);
+        }
 
-        printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
     }
 }
