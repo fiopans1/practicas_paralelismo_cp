5e255b4a69c176f0bc6d70dd1a690fa6605140ff
fiopans1 <dsuaran@gmail.com>
2022-04-14 19:11:40 +0200

    TERMINADO

--- p3/p3.c
+++ p3/p3.c
@@ -2,11 +2,11 @@
 #include <sys/time.h>
 #include <mpi.h>
 #include <stdlib.h>
-#define DEBUG 1
+#define DEBUG 0
 
 
 
-#define N 5
+#define N 1024
 
 
 int main(int argc, char *argv[] ) {
@@ -15,11 +15,11 @@ int main(int argc, char *argv[] ) {
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int i, j;
-    float *matrix_Aux = (float *) malloc(sizeof(float)*(N/numprocs)*N);//luego adaptar tamaño de matrix_Aux a lo que recibe cada proceso
+    float *matrix_Aux = (float *) malloc(sizeof(float)*(N/numprocs)*N);
     float *vector=(float *) malloc(sizeof(float)*N);
-    float *result=(float *) malloc(sizeof(float)*N);
+    float *result=(float *) malloc(sizeof(float)*N);//este se podria optimizar un poco más el tamaño pero eso ya a mayores
 
-    struct timeval  tv1, tv2,tv3,tv4,tv5,tv6;
+    struct timeval  tv1, tv2,tv3,tv4,tv5,tv6,tv7,tv8;
     if(rank==0){
         float *matrix = (float *) malloc(sizeof(float)*N*N);
         /* Initialize Matrix and Vector */
@@ -29,15 +29,18 @@ int main(int argc, char *argv[] ) {
                 matrix[i*N+j] = i+j;
             }
         }
+        gettimeofday(&tv5, NULL);
         MPI_Bcast(vector,N,MPI_FLOAT,0,MPI_COMM_WORLD);
         MPI_Scatter(matrix,(N/numprocs)*N,MPI_FLOAT,matrix_Aux,(N/numprocs)*N,MPI_FLOAT,0,MPI_COMM_WORLD);
+        gettimeofday(&tv3, NULL);
 
-        for(i=((numprocs*(N/numprocs))-1);i<N;i++) {
+        for(i=((numprocs*(N/numprocs))-1);i<N;i++) {//calcula el resto
             result[i]=0;
             for(j=0;j<N;j++) {
                 result[i] += matrix[i*N+j]*vector[j];
             }
         }
+        gettimeofday(&tv4, NULL);
         free(matrix);
     }else{
         MPI_Bcast(vector,N,MPI_FLOAT,0,MPI_COMM_WORLD);
@@ -55,11 +58,17 @@ int main(int argc, char *argv[] ) {
     if(rank==0) gettimeofday(&tv2, NULL);
     if(rank==0){
         float *result2=(float *) malloc(sizeof(float)*N);
+        gettimeofday(&tv6, NULL);
         MPI_Gather(result,(N/numprocs),MPI_FLOAT,result2,(N/numprocs),MPI_FLOAT,0,MPI_COMM_WORLD);
+        gettimeofday(&tv7, NULL);
         for(i=((numprocs*(N/numprocs))-1);i<N;i++){
             result2[i]=result[i];
         }
         int microseconds = (tv2.tv_usec - tv1.tv_usec)+ 1000000 * (tv2.tv_sec - tv1.tv_sec);
+        int microseconds2 = (tv4.tv_usec - tv3.tv_usec)+ 1000000 * (tv4.tv_sec - tv3.tv_sec);
+        int microseconds3 = (tv3.tv_usec - tv5.tv_usec)+ 1000000 * (tv3.tv_sec - tv5.tv_sec);
+        int microseconds4 = (tv7.tv_usec - tv6.tv_usec)+ 1000000 * (tv7.tv_sec - tv6.tv_sec);
+
 
         //Display result
         if (DEBUG){
@@ -67,7 +76,9 @@ int main(int argc, char *argv[] ) {
             printf(" %f \t ",result2[i]);
         }
         } else {
-        printf ("Time (seconds) = %lf\n", (double) microseconds/1E6);
+        printf ("Tiempo de trabajo de cada proceso (seconds) = %lf\n", (double) microseconds/1E6);
+        printf ("Tiempo restante proceso 0 (seconds) = %lf\n", (double) microseconds2/1E6);
+        printf ("Tiempo comunicaciones (seconds) = %lf\n", (double) (microseconds3+microseconds4)/1E6);
         }    
         free(result2);
         printf("\n");
